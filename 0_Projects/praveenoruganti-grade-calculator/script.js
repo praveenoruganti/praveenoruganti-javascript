e406e6f24a7266010b5c685127d1e6205d79d8ea
Praveen Oruganti <praveenorugantitech@gmail.com>
2020-12-27 20:04:18 +0530

    All About JavaScript

--- 0_Projects/praveenoruganti-grade-calculator/script.js
+++ 0_Projects/praveenoruganti-grade-calculator/script.js
@@ -26,7 +26,9 @@ let getMarks = () => {
       document.getElementById("percentage").innerHTML = null;
     } else if (percentage < 60) {
       grade = "B";
-    } else if (percentage > 70 && percentage < 90) {
+    } else if (percentage > 60 && percentage < 70) {
+      grade = "B+";
+    }else if (percentage > 70 && percentage < 90) {
       grade = "A";
     } else {
       grade = "A+";
