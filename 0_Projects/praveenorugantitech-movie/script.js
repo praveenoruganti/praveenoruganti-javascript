d63db757f873ea99bff698c8ae9e00dd39671143
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-06 17:52:40 +0530

    updated

--- 0_Projects/praveenorugantitech-movie/script.js
+++ 0_Projects/praveenorugantitech-movie/script.js
@@ -21,7 +21,7 @@ function showMovies(movies){
         const{title,poster_path,vote_average,overview} = movie;
         const movieEL = document.createElement('div');
         movieEL.classList.add('movie')
-        const imagePath = poster_path !== null ? `<img src="${IMG_PATH + poster_path}" alt = "${title}">` : `<img src="noposter.jpg" alt = "No Title">`;
+        const imagePath = poster_path !== null ? `<img src="${IMG_PATH + poster_path}" alt = "${title}">` : `<img src="noposter.jpg">`;
         movieEL.innerHTML = `
         ${imagePath}
                 <div class="movie-info">
